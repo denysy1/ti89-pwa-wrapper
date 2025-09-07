8388a91638fc70c8e248e79bb0c34e77bd9370aa
denysy1 <79426502+denysy1@users.noreply.github.com>
2025-09-07 16:45:15 -0700

    Icon definition updates

--- sw.js
+++ sw.js
@@ -1,2 +1,42 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
+const CACHE_NAME = 'ti89-pwa-cache-v1.0.0';
+const URLS_TO_CACHE = [
+  '/ti89-pwa-wrapper/',
+  '/ti89-pwa-wrapper/index.html',
+  '/ti89-pwa-wrapper/manifest.webmanifest',
+  '/ti89-pwa-wrapper/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
